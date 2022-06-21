b6c20c7985fc3785c9e94f75945abad5e479d0da
gayibovih <ismat.qayibov@gmail.com>
2022-06-21 20:24:35 +0300

    update main.go

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
 
+func welcome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("welcome"))
+}
+
+func byebye(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("byebye"))
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/hello", welcome)
+	r.Get("/bye", byebye)
+
+	http.ListenAndServe(":3000", r)
 }
