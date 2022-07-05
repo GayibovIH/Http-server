b3ce0fd892aca60b63a6c373c3b9ce61a5f796bc
gayibovih <ismat.qayibov@gmail.com>
2022-07-05 20:45:23 +0300

    handler.go

--- internal/handler/httpHandler/handler.go
+++ internal/handler/httpHandler/handler.go
@@ -1,17 +1,30 @@
 package httpHandler
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type HttpHandler struct {
 	fib map[int]int
 }
 
 func (hh HttpHandler) calculate(num int) {
+	var fibonacci = []int{0, 1}
+	for i := 2; i < num; i++ {
+		num2 := fibonacci[i-1] + fibonacci[i-2]
+		fibonacci = append(fibonacci, num2)
+	}
 
+	hh.fib[num] = fibonacci[num-1]
 }
 
 func (hh HttpHandler) get(num int) int {
-	return 0
+	var value, okey = hh.fib[num]
+	if okey {
+		return value
+	} else {
+		return -1
+	}
 }
 
 func (hh HttpHandler) CalculateHandle(rw http.ResponseWriter, r *http.Request) {
